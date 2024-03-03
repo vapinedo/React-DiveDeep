434a82e1046d071dc0acea0b1541e4e7751a4ff3
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-03-03 09:59:02 -0500

    Basic recursion function

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-// React style code
-const countApp = {
-    getCount: () => {
-        const countElement = document.getElementById("count");
-        return Number(countElement.textContent);
-    },
-    setCount: (val) => {
-        const countElement = document.getElementById("count");
-        countElement.textContent = val;
-    }
-};
+function countDown(num) {
+    console.log(num);
 
-
-// Your code using React's code
-function setCount() {
-    let count = countApp.getCount();
-    if (count >= 5) {
-        countApp.setCount(0);
-    } else {
-        countApp.setCount(count + 1);
-    }
+    if (num === 0 ) return;
+    
+    countDown(num - 1);
 }
+
+countDown(5);
