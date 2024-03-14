ae9a4bb23733f4e2d8ec49499d34b0421aa5fdd5
vpinedo-neostella <victor.pinedo@neostella.com>
2024-03-14 11:29:25 -0500

    Components

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
 
 function App() {
-    return (
-        React.createElement("article", null,
-            React.createElement("h2", null, "Counter "),
-            React.createElement("p", null, "You clicked 1 times"),
-            React.createElement("button", null, "Click me"))
-    )
+  return React.createElement(
+    "section",
+    null,
+    React.createElement("h1", null, "Counters"),
+    React.createElement("section", null, React.createElement(Counter, null))
+  );
+}
+
+function Counter() {
+  return React.createElement(
+    "article",
+    null,
+    React.createElement("h2", null, "Counter "),
+    React.createElement("p", null, "You clicked 1 times"),
+    React.createElement("button", null, "Click me")
+  );
 }
