2db2c938656c8b680c55a843dac374372140e7cb
vpinedo-neostella <victor.pinedo@neostella.com>
2024-03-19 10:15:50 -0500

    Template Logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
+let counterName = "One";
 
 function App() {
   return React.createElement(
@@ -10,20 +11,35 @@ function App() {
     React.createElement(
       "section",
       null,
-      React.createElement(Counter, { name: "One" }),
-      React.createElement(Counter, { name: "Two" })
+      counterName === "One"
+        ? React.createElement(Counter, { name: counterName })
+        : React.createElement(Counter2, { name: counterName })
     )
   );
 }
 
-function Counter(props) {
-  console.log(props);
-  props.name = "Valp";
+function Counter({ name }) {
   return React.createElement(
     "article",
     null,
-    React.createElement("h2", null, "Counter ", props.name),
+    React.createElement("h2", null, "Counter ", name),
     React.createElement("p", null, "You clicked 1 times"),
     React.createElement("button", null, "Click me")
   );
+}
+
+function Counter2({ name }) {
+  return React.createElement(
+    "article",
+    null,
+    React.createElement("h2", null, "Counter ", name),
+    React.createElement("p", null, "You clicked 1 times"),
+    React.createElement("button", null, "Click me")
+  );
+}
+
+function rerender() {
+  console.log("Rerender...");
+  counterName = "Two";
+  root.render(React.createElement(App));
 }
